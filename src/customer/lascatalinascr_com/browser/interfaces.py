eb11123674703cfbcb72dde7c1ced6c49743ca2c
Jens Krause <jens@propertyshelf.com>
2014-09-08 22:43:02 +0700

    installable product with registered browser layer and basic profile

---
+++ src/customer/lascatalinascr_com/browser/interfaces.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# zope imports
+from zope.interface import Interface
+
+
+class ICustomerLascatalinascrComLayer(Interface):
+    """Marker interface for the Browserlayer."""
