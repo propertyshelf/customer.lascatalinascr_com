00f1c5500fb024b873fc4676f828b418830c9344
Jens Krause <jens@propertyshelf.com>
2014-09-15 19:38:25 +0700

    render for now a static search result of the @@ajaxListingSearch View

--- src/customer/lascatalinascr_com/portlets/ajax_search.py
+++ src/customer/lascatalinascr_com/portlets/ajax_search.py
@@ -1,18 +1,83 @@
 # -*- coding: utf-8 -*-
 """View for AjaxSearch"""
-
+from plone.memoize.view import memoize
 from Products.Five.browser import BrowserView, pagetemplatefile
+from zope.component import queryMultiAdapter
+from zope.traversing.browser.absoluteurl import absoluteURL
+
+#plone.mls.listing imports
+from plone.mls.core.navigation import ListingBatch
+from plone.mls.listing.api import prepare_search_params, search
 
 class ajaxSearch(BrowserView):
     """Deliver search results for ajax calls"""
     index = pagetemplatefile.ViewPageTemplateFile("templates/ajax_template.pt")
 
+    _listings = None
+    _batching = None
+
+
     def __init__(self, context, request):
+        super(ajaxSearch, self).__init__(context, request)
         self.context = context
         self.request = request
+        self.update()
+        
 
     def __call__(self):
         return self.render()
 
+    def update(self):
+        self.portal_state = queryMultiAdapter((self.context, self.request),
+                                                name='plone_portal_state')
+        self.context_state = queryMultiAdapter((self.context, self.request),
+                                               name='plone_context_state')
+        request_params = self._get_params
+        self._get_listings(request_params)
+
+    @property
+    def limit(self):
+        return 15
+
     def render(self):
+        """Prepare view related data.""" 
         return self.index()
+
+    @property
+    def _get_params(self):
+        """map MLS search with custom UI"""
+        params={}
+        return params
+
+    def _get_listings(self, params):
+        """Query the recent listings from the MLS."""
+        search_params = {
+            'limit': self.limit,
+            'offset': self.request.get('b_start', 0),
+            'lang': self.portal_state.language(),
+            'agency_listings': False
+        }
+        search_params.update(params)
+        results, batching = search(search_params, context=self.context)
+        self._listings = results
+        self._batching = batching
+
+    @property
+    @memoize
+    def listings(self):
+        """Return listing results."""
+        return self._listings
+
+    @memoize
+    def view_url(self):
+        """Generate view url."""
+        if not self.context_state.is_view_template():
+            return self.context_state.current_base_url()
+        else:
+            return absoluteURL(self.context, self.request) + '/'
+
+    @property
+    def batching(self):
+        return ListingBatch(self.listings, self.limit,
+                            self.request.get('b_start', 0), orphan=1,
+                            batch_data=self._batching)
