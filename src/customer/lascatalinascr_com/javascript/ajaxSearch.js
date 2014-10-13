f839d753897ad1a651cc5d9bba1df063b9656373
Jens Krause <jens@propertyshelf.com>
2014-10-13 19:15:47 +0700

    add javascript resource folder and register js-file for ajaxSearch

---
+++ src/customer/lascatalinascr_com/javascript/ajaxSearch.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    //if the AjaxFilter Portlet is available
+    // execute the AjaxSearch
+    if($('.aJaXFilter').length>0){
+
+        console.log('we are filtering');
+
+    }
+
+})
