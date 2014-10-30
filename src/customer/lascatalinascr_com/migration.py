8854ab61bd584c2954b0fe9149ad582c5cd7951f
Jens Krause <jens@propertyshelf.com>
2014-10-31 02:28:25 +0700

    add upgrade step to version 1002

--- src/customer/lascatalinascr_com/migration.py
+++ src/customer/lascatalinascr_com/migration.py
@@ -19,3 +19,11 @@ def migrate_to_1001(context):
     setup.runImportStepFromProfile(PROFILE_ID, 'jsregistry')
     setup.runImportStepFromProfile(PROFILE_ID, 'cssregistry')
     setup.runImportStepFromProfile(PROFILE_ID, 'portlets')
+
+def migrate_to_1002(context):
+    """Migrate from 1001 to 1002.
+    * Register embedding portlet.
+    """
+    site = getUtility(IPloneSiteRoot)
+    setup = getToolByName(site, 'portal_setup')
+    setup.runImportStepFromProfile(PROFILE_ID, 'portlets')
